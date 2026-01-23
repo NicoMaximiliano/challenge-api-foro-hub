118bce62c659bed9e2128207d843bfedc30b3f17
NicoMaximiliano <nicolas.orellano25@outlook.com.ar>
2026-01-22 23:48:26 -0300

    Subiendo el proyecto

---
+++ src/main/java/com/nicode/challenge_api_foro_hub/ChallengeApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.nicode.challenge_api_foro_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApiForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicode/challenge_api_foro_hub/ChallengeApiForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicode.challenge_api_foro_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApiForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
